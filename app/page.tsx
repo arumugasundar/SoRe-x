dade2a2aa8f9ab18f799cc300edd1813ed1d914c
arumugasundar <arumugasundar29@gmail.com>
2024-05-11 20:26:55 +0530

    feat : shadcn ui setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+      Landing Page
+      <Button> Login </Button>
     </main>
   );
 }
--- data/user.ts
+++ data/user.ts
@@ -3,7 +3,6 @@ import { db } from "@/lib/db";
 export const getUsers = async () => {
     try{
         const user = await db.user.findMany();
-        console.log('user :', user)
         return user;
     } catch {
         return null;
