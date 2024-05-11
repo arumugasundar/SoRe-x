2decd418d3107bec53dfc2284bb1eaec96259dfc
pavansv24 <pavansv24@gmail.com>
2024-05-11 18:32:51 +0530

    updates on login page

--- app/auth/login1/page.tsx
+++ app/auth/login1/page.tsx
@@ -12,13 +12,7 @@ const Page = () => {
                         </label>
                         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your phone number" />
                     </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Email
-                        </label>
-                        <input className="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your email address"/>
-                            
-                    </div>
+                    
                     <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             OTP
