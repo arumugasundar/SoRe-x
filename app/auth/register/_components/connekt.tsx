ffe4cdaa60612b743accabaa32ed2d4ac257b87e
arumugasundar <arumugasundar29@gmail.com>
2024-05-12 11:06:35 +0530

    refactor : ui works

---
+++ app/auth/register/_components/connekt.tsx
@@ -0,0 +1,214 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+import toast, { Toaster } from "react-hot-toast";
+import { EyeIcon, EyeOffIcon, MailIcon, PhoneIcon } from "lucide-react";
+
+const Component = () => {
+
+    const router = useRouter();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [emailOtp, setEmailOtp] = useState("");
+    const [phone, setPhone] = useState("");
+    const [phoneOtp, setPhoneOtp] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [gender, setGender] = useState("");
+    const [age, setAge] = useState("");
+
+    const sendOtp = async (type: string, value: string) => {
+    
+        if (type && value) {
+          const myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
+          const raw = JSON.stringify({ "type": `${type}`, "value": `${value}`});
+          const requestOptions: any = { method: "POST", headers: myHeaders, body: raw, redirect: "follow" }; 
+          await fetch("/api/otp", requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
+          toast("otp sent to your email.", { className: "toast-success" });
+        } else {
+          toast("oops! something went wrong.", { className: "toast-error" });
+        }
+      };
+    
+      const submitHandler = async () => {
+        
+        if(name && gender && age && email && emailOtp && phone && phoneOtp && password && confirmPassword){
+          let payload = { name, gender, age, email, emailOtp, phone, phoneOtp, password, confirmPassword };
+          
+          const myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
+          const raw = JSON.stringify(payload);
+          const requestOptions: any = { method: "POST", headers: myHeaders, body: raw, redirect: "follow" };
+    
+          let response: any = await fetch("/api/users", requestOptions);
+          response = await response.json();
+          
+          if(response?.id){
+            router.push('/auth/login');
+          }
+        } else {
+          toast("invalid data found", { className: "toast-error" });
+        }
+      };
+
+  return (
+    <Card className="bg-zinc-700/75">
+      <CardHeader>
+        <CardTitle> Connekt </CardTitle>
+        <CardDescription>
+        Be the change to bring the change. Have a vibrant solution to a sensible problem? Here&apos;s the platform to pitch and shape your ideas.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-wrap justify-around">
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="name"> Name </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e?.target?.value)}
+          />
+        </div>
+        <div className="w-3/12 m-1 p-1">
+          <Label htmlFor="gender"> Gender </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="gender"
+            value={gender}
+            onChange={(e) => setGender(e?.target?.value)}
+          />
+        </div>
+        <div className="w-3/12 m-1 p-1">
+          <Label htmlFor="age"> Age </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="age"
+            value={age}
+            onChange={(e) => setAge(e?.target?.value)}
+          />
+        </div>
+
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="email"> Email </Label>
+          <Input
+            type="email"
+            className="shadow-lg border-2 border-indigo-600"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e?.target?.value)}
+            suffix={
+              <MailIcon
+                className="cursor-pointer"
+                onClick={async () => await sendOtp("email", email)}
+              />
+            }
+          />
+        </div>
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="email_otp"> Email Verification OTP </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="email_otp"
+            value={emailOtp}
+            onChange={(e) => setEmailOtp(e?.target?.value)}
+          />
+        </div>
+
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="phone"> Phone </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e?.target?.value)}
+            suffix={
+              <PhoneIcon
+                className="cursor-pointer"
+                onClick={async () => await sendOtp("phone", phone)}
+              />
+            }
+          />
+        </div>
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="phone_otp"> Phone Verification OTP </Label>
+          <Input
+            type="text"
+            className="shadow-lg border-2 border-indigo-600"
+            id="phone_otp"
+            value={phoneOtp}
+            onChange={(e) => setPhoneOtp(e?.target?.value)}
+          />
+        </div>
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="password"> Password </Label>
+          <Input
+            className="shadow-lg border-2 border-indigo-600"
+            type={showPassword ? "text" : "password"}
+            id="password"
+            suffix={
+              showPassword ? (
+                <EyeIcon
+                  className="select-none cursor-pointer"
+                  onClick={() => setShowPassword(false)}
+                />
+              ) : (
+                <EyeOffIcon
+                  className="select-none cursor-pointer"
+                  onClick={() => setShowPassword(true)}
+                />
+              )
+            }
+            value={password}
+            onChange={(e) => setPassword(e?.target?.value)}
+          />
+        </div>
+        <div className="w-5/12 m-1 p-1">
+          <Label htmlFor="password"> Confirm Password </Label>
+          <Input
+            className="shadow-lg border-2 border-indigo-600"
+            type={showConfirmPassword ? "text" : "password"}
+            id="confirm_password"
+            suffix={
+              showConfirmPassword ? (
+                <EyeIcon
+                  className="select-none cursor-pointer"
+                  onClick={() => setShowConfirmPassword(false)}
+                />
+              ) : (
+                <EyeOffIcon
+                  className="select-none cursor-pointer"
+                  onClick={() => setShowConfirmPassword(true)}
+                />
+              )
+            }
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e?.target?.value)}
+          />
+        </div>
+      </CardContent>
+      <CardFooter className="w-full flex justify-end">
+        <Button className="bg-indigo-600 text-white" onClick={async () => await submitHandler()}> Register </Button>
+      </CardFooter>
+      <Toaster />
+    </Card>
+  )
+}
+
+export default Component
--- app/home/_components/add-issue.tsx
+++ app/home/_components/add-issue.tsx
@@ -6,6 +6,16 @@ import { Label } from "@/components/ui/label"
 import { Plus } from 'lucide-react'
 import { useState } from "react"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 const Component = () => {
 
     const [ description, setDescription ] = useState('');
@@ -13,20 +23,62 @@ const Component = () => {
     const [ severity, setSeverity ] = useState('');
     const [ severityReason, setSeverityReason ] = useState('');
     const [ issuePersistsSince, setIssuePersistsSince ] = useState('');
+    const [ addLocation, setAddLocation ] = useState('');
+
+    const [place, setPlace ] = useState('Select Place');
+    const [locality, setLocality ] = useState('Select Locality');
+    // const [locality, setLocality ] = useState('Select Locality');
+    // const [locality, setLocality ] = useState('Select Locality');
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="w-full flex justify-end">
-            <Button variant="outline" className="bg-indigo-600 text-white text-base m-4 p-2"> <Plus size={20} /> Report Issue </Button>
+            <Button variant="outline" size={"lg"} className="bg-indigo-600 text-white text-2xl mx-auto mt-44"> <Plus size={40} /> Report Issue </Button>
         </div>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[800px] h-[540px] overflow-auto">
         <DialogHeader>
           <DialogTitle> Issue Report </DialogTitle>
-          <DialogDescription> tagline - to convey the expectations of how the issue reporting should be </DialogDescription>
+          <DialogDescription className="mt-2"> tagline - to convey the expectations of how the issue reporting should be </DialogDescription>
         </DialogHeader>
 
-        <div className="w-full m-1 p-1">
+        <div className="flex w-full gap-2">
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="bg-indigo-600 p-1 m-1 rounded-sm"> {place} </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onClick={() => setPlace('Bengaluru Urban')}> Bengaluru Urban </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setPlace('Bengaluru Rural')}> Bengaluru Rural </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="bg-indigo-600 p-1 m-1 rounded-sm"> {locality} </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onClick={() => setLocality('Koramangala')}> Koramangala </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setLocality('Indiranagar')}> Indiranagar </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        
+
+
+
+        <div className="flex flex-wrap justify-around">
+        <div className="w-5/12 m-1 p-1">
+            <Label htmlFor="image"> Upload Image </Label>
+            <Input
+              type="file"
+              className="shadow-lg border-2 border-indigo-600"
+              id="image"
+            />
+          </div>
+
+        <div className="w-5/12 m-1 p-1">
             <Label htmlFor="description"> Description </Label>
             <Input
               type="text"
@@ -37,7 +89,7 @@ const Component = () => {
             />
           </div>
 
-          <div className="w-full m-1 p-1">
+          <div className="w-5/12 m-1 p-1">
             <Label htmlFor="department"> Department </Label>
             <Input
               type="text"
@@ -48,7 +100,7 @@ const Component = () => {
             />
           </div>
 
-          <div className="w-full m-1 p-1">
+          <div className="w-5/12 m-1 p-1">
             <Label htmlFor="severity"> Severity </Label>
             <Input
               type="text"
@@ -59,7 +111,7 @@ const Component = () => {
             />
           </div>
 
-          <div className="w-full m-1 p-1">
+          <div className="w-5/12 m-1 p-1">
             <Label htmlFor="severity_reason"> severity Reason </Label>
             <Input
               type="text"
@@ -70,7 +122,7 @@ const Component = () => {
             />
           </div>
 
-          <div className="w-full m-1 p-1">
+          <div className="w-5/12 m-1 p-1">
             <Label htmlFor="issue_persists_since"> Issue Persists Since </Label>
             <Input
               type="text"
@@ -81,8 +133,20 @@ const Component = () => {
             />
           </div>
 
+          <div className="w-11/12 m-1 p-1">
+            <Label htmlFor="add_location"> Add Location </Label>
+            <Input
+              type="text"
+              className="shadow-lg border-2 border-indigo-600"
+              id="add_location"
+              value={addLocation}
+              onChange={(e) => setAddLocation(e?.target?.value)}
+            />
+          </div>
+        </div>
+
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" className="bg-indigo-600 text-white">Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
